8a3d77561a835bbc1d614762e73f156e40a66cbf
Alecs10 <73397938+Alecs10@users.noreply.github.com>
2021-06-08 00:10:56 +0300

    started the simulator, completed IF+OF+1/6 of EX

--- Simulator/Simulator/Form1.cs
+++ Simulator/Simulator/Form1.cs
@@ -12,10 +12,26 @@ namespace Simulator
 {
     public partial class Form1 : Form
     {
+
+        Simulator simulator;
+
+        List<TextBox> registers;
+
         public Form1()
         {
+            simulator = new Simulator();
+            registers = new List<TextBox>();
             InitializeComponent();
             CenterToScreen();
+
+           foreach(var tb in this.Controls)
+            {
+                if(tb is TextBox)
+                {
+                    ((TextBox)tb).Text = "0";
+                }
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,20 +39,132 @@ namespace Simulator
             
         }
 
-        private void loadfileBTN_Click(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
-            Assembler.filePath = Assembler.loadFile();
-            List<List<string>> asmmatrix = Assembler.parseFile(Assembler.filePath);
 
-            Assembler.displayContentsOnTextbox(asmmatrix, textBox1);
-
-            assembleBTN.Enabled = true;
         }
 
-        private void assembleBTN_Click(object sender, EventArgs e)
+        private void label14_Click(object sender, EventArgs e)
         {
-            Assembler.assemble();
+
         }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegPC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegMDR_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegFlag_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegSP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegIVR_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegIR_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegADR_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegDec_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //private void loadfileBTN_Click(object sender, EventArgs e)
+        //{
+        //    Assembler.filePath = Assembler.loadFile();
+        //    List<List<string>> asmmatrix = Assembler.parseFile(Assembler.filePath);
+
+        //    Assembler.displayContentsOnTextbox(asmmatrix, textBox1);
+
+        //    assembleBTN.Enabled = true;
+        //}
+
+        //private void assembleBTN_Click(object sender, EventArgs e)
+        //{
+        //    Assembler.assemble();
+        //}
+
+        //private void textBox1_TextChanged(object sender, EventArgs e)
+        //{
+
+        //}
 
     }
 }
---
+++ Simulator/Simulator/Simulator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    class Simulator
+    {
+        public List<ushort> registers;
+        public byte FLAG; 
+        public ushort PC, DBUS, T, IVR, ADR, MDR, IR, RBUS, SBUS, ALU;
+        public ushort SP = 65535;
+
+        public byte[] memoryLocations = new byte[1 << 16];
+        
+
+
+
+        public Simulator()
+        {
+            registers = new List<ushort>();
+            for(short i = 0; i < 16; i++)
+            {
+                registers.Add(0);
+            }
+            loadFileToMemory(@"C:\Users\popa_\Desktop\test.bin");
+        }
+
+        public void loadFileToMemory(string filePath)
+        {
+            var fileContent = File.ReadAllBytes(filePath);
+            int i = 0;
+            foreach(var octet in fileContent)
+            {
+                memoryLocations[i] = octet;
+                i++;
+            }
+        }
+
+
+    }
+}
